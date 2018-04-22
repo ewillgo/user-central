5e82842448f611132e3c7e510c2c9ab174ac457c
ewill.leung@foxmail.com <will89liangwenwei>
2018-04-22 22:18:27 +0800

    ...

---
+++ core/src/main/java/org/trianglex/usercentral/session/AccessToken.java
@@ -0,0 +1,32 @@
+package org.trianglex.usercentral.session;
+
+public class AccessToken {
+
+    private String userId;
+    private String sessionId;
+    private long timestamp;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
---
+++ core/src/main/java/org/trianglex/usercentral/session/AccessTokenProperties.java
@@ -0,0 +1,28 @@
+package org.trianglex.usercentral.session;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
+
+@ConfigurationProperties(prefix = "usercentral.accessToken")
+public class AccessTokenProperties {
+
+    private Duration tokenMaxAge;
+    private String tokenEncryptKey;
+
+    public Duration getTokenMaxAge() {
+        return tokenMaxAge;
+    }
+
+    public void setTokenMaxAge(Duration tokenMaxAge) {
+        this.tokenMaxAge = tokenMaxAge;
+    }
+
+    public String getTokenEncryptKey() {
+        return tokenEncryptKey;
+    }
+
+    public void setTokenEncryptKey(String tokenEncryptKey) {
+        this.tokenEncryptKey = tokenEncryptKey;
+    }
+}
