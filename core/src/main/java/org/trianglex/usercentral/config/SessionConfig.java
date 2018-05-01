456e03a01d2e333d4f5e655729cae88ea3e686ab
ewill.leung@foxmail.com <will89liangwenwei>
2018-05-01 15:14:11 +0800

    ...

--- core/src/main/java/org/trianglex/usercentral/config/SessionConfig.java
+++ core/src/main/java/org/trianglex/usercentral/config/SessionConfig.java
@@ -3,15 +3,17 @@ package org.trianglex.usercentral.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.session.web.http.SessionEventHttpSessionListenerAdapter;
 import org.trianglex.usercentral.session.AccessTokenProperties;
+import org.trianglex.usercentral.session.SessionCookieProperties;
 import org.trianglex.usercentral.session.TicketProperties;
 import org.trianglex.usercentral.session.UserSessionListener;
 
 import java.util.Arrays;
 
 @Configuration
-@Import({TicketProperties.class, AccessTokenProperties.class})
+@Import({SessionCookieProperties.class, TicketProperties.class, AccessTokenProperties.class})
 public class SessionConfig {
 
     @Bean
@@ -22,6 +24,16 @@ public class SessionConfig {
     @Bean
     public SessionEventHttpSessionListenerAdapter sessionEventHttpSessionListenerAdapter(UserSessionListener userSessionListener) {
         return new SessionEventHttpSessionListenerAdapter(Arrays.asList(userSessionListener));
+    }
+
+    @Bean
+    public DefaultCookieSerializer defaultCookieSerializer(SessionCookieProperties properties) {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookiePath(properties.getCookiePath());
+        serializer.setCookieMaxAge((int) properties.getCookieMaxAge().getSeconds());
+        serializer.setUseHttpOnlyCookie(properties.isUseHttpOnlyCookie());
+        serializer.setCookieName(properties.getCookieName());
+        return serializer;
     }
 
 }
