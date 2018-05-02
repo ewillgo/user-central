8b65cfa0fa92b36aeda6ec1cd0853ad41792fd56
liangwenwei <liangwenwei@tuandai.com>
2018-05-02 18:32:10 +0800

    ...

--- core/src/main/java/org/trianglex/usercentral/config/UserCentralConfig.java
+++ core/src/main/java/org/trianglex/usercentral/config/UserCentralConfig.java
@@ -3,11 +3,11 @@ package org.trianglex.usercentral.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.trianglex.common.config.WebConfig;
-import org.trianglex.common.security.WebSecurityConfig;
 import org.trianglex.common.security.XssRequestFilter;
+import org.trianglex.usercentral.config.cors.UserCentralWebSecurityConfig;
 
 @Configuration
-@Import({WebConfig.class, WebSecurityConfig.class, XssRequestFilter.class})
+@Import({WebConfig.class, UserCentralWebSecurityConfig.class, XssRequestFilter.class})
 public class UserCentralConfig {
 
 }
---
+++ core/src/main/java/org/trianglex/usercentral/config/cors/UserCentralWebSecurityConfig.java
@@ -0,0 +1,47 @@
+package org.trianglex.usercentral.config.cors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.trianglex.usercentral.config.cors.CorsProperties;
+
+@Configuration
+@Import(CorsProperties.class)
+public class UserCentralWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CorsProperties corsProperties;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**")
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(corsProperties.getAllowedOrigins());
+        configuration.setAllowedMethods(corsProperties.getAllowedMethods());
+        configuration.setAllowCredentials(corsProperties.isAllowCredentials());
+        configuration.setAllowedHeaders(corsProperties.getAllowedHeaders());
+        configuration.setMaxAge(corsProperties.getMaxAge());
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration(corsProperties.getPath(), configuration);
+        return source;
+    }
+}
