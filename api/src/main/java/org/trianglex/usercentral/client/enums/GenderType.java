6f8debaa7047d02c6f29dde51355287e9ec7782b
ewill.leung@foxmail.com <will89liangwenwei>
2018-05-20 15:29:56 +0800

    ...

---
+++ api/src/main/java/org/trianglex/usercentral/client/enums/GenderType.java
@@ -0,0 +1,24 @@
+package org.trianglex.usercentral.client.enums;
+
+public enum GenderType {
+
+    SECRET(0, "保密"),
+    MALE(1, "男"),
+    FEMALE(2, "女");
+
+    private Integer code;
+    private String message;
+
+    GenderType(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
